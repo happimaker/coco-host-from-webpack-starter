62b7dd3f17397a4c3455408670d6aacb5942ad2b
happimaker <58175409+happimaker@users.noreply.github.com>
2019-12-10 08:27:13 +0800

    refactor:responsive

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,3 +1,10 @@
 import '../styles/index.scss';
 
+var navbarElement = document.getElementById('navbar');
+
+document.getElementById('navbar-trigger').onclick = function() {
+	navbarElement.style.display = navbarElement.style.display === 'block' ? 'none' : 'block';
+};
+
+
 console.log('webpack starterkit');
